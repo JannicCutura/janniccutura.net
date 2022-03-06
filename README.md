e7de1ce90d6a5cba0f4ac45bd4ced27164da1c81
jannic <Janniccutura@gmail.com>
2022-03-06 11:17:06 +0100

    automated hugo -D

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 1. open poewrshell admin in this folder `hugo2\startrt-academic`
 2. `hugo server` to view locally
+3. Delete all files from `public`
 3. `hugo -D` to generate files to `public`
 4. copy everything from public to S3. (automate this). Careful to not forget folders, drag and drop!!
--- deploy_to_s3.py
+++ deploy_to_s3.py
@@ -1,12 +1,27 @@
 import boto3
 import os
 from tqdm import tqdm
+import shutil
+import subprocess
 
+# define resrouces
 s3 = boto3.resource('s3')
 BUCKET = "janniccutura.net"
 
-os.listdir()
 
+
+# delete all files
+os.listdir('public')
+shutil.rmtree('public', ignore_errors=False, onerror=None)
+
+subprocess.Popen("del /S *", cwd='C:/Users/janni/Dropbox/website/janniccutura.net/public')
+
+
+# genereate production files
+subprocess.Popen("hugo -D", cwd='C:/Users/janni/Dropbox/website/janniccutura.net')
+
+
+# upload to s3
 import os
 website_files = []
 for path, currentDirectory, files in os.walk(os.getcwd()+'\public'):
